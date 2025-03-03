1320a36474f9432b2b76f1a5f9428eea16b78546
Luigi Cocozza <luigi.cocozza@shadow.tech>
2025-03-03 13:28:18 +0100

    update version handler

--- cargs/sources/callbacks/handlers/version_handler.c
+++ cargs/sources/callbacks/handlers/version_handler.c
@@ -1,28 +1,23 @@
 #include "cargs/utils.h"
 #include "cargs/types.h"
+
 #include <string.h>
-
 #include <stdio.h>
+#include <stdlib.h>
 
 
-// void	*cargs_version_handler(cargs_t *cargs, char *arg, void *result)
-// {
-// 	(void)arg;
-// 	(void)result;
-// 	printf("%s v%s\n", cargs->program_name, cargs->program_version);
-// 	exit(EXIT_SUCCESS);
-// }
-
-// void cargs_print_version(cargs_t *cargs)
-// {
-// 	cargs_version_handler(cargs, NULL, NULL);
-// }
-
-
-int version_handler(cargs_option_t *options, char *arg)
+void	cargs_print_version(cargs_t *cargs)
 {
-	(void)options;
+	printf("%s v%s\n", cargs->program_name, cargs->version);
+	if (cargs->description != NULL)
+		printf("\n%s\n", cargs->description);
+}
+
+int version_handler(cargs_t *cargs, cargs_option_t *options, char *arg)
+{
 	UNUSED(arg);
-	printf("version handler\n");
+	cargs_print_version(cargs);
+	if (options->flags & FLAG_EXIT)
+		exit(EXIT_SUCCESS);
 	return (CARGS_SUCCESS);
 }
