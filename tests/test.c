71f8179c8c33c04d70cd78c173651cccff05a604
Luigi Cocozza <luigi.cocozza@shadow.tech>
2025-03-05 13:54:45 +0100

    add [api]: cargs_free

--- tests/test.c
+++ tests/test.c
@@ -12,8 +12,7 @@ int custom_handler(cargs_t *cargs, cargs_option_t *options, char *arg) {
 CARGS_OPTIONS(
     toto,
     OPTION_INT('b', "beta", "Beta value", DEFAULT(21)),
-    OPTION_INT('a', "alpha", "Alpha value",
-                CONFLICTS("beta")),
+    OPTION_INT('a', "alpha", "Alpha value"),
 );
 
 CARGS_OPTIONS(
@@ -22,21 +21,16 @@ CARGS_OPTIONS(
     VERSION_OPTION(),
 
     GROUP_START("Network", FLAGS(FLAG_EXCLUSIVE)),
-    OPTION_INT('p', "port", "Port number",
-                DEFAULT(8080),
-                CHOICES_INT(80, 443, 8080)),
-    OPTION_STRING('H', "host", "Host name",
-                CHOICES_STRING("localhost", "127.0.0.1", "::1")),
+    OPTION_INT('p', "port", "Port number"),
+    OPTION_STRING('H', "host", "Host name"),
     GROUP_END(),
 
     SUBCOMMAND("toto", toto),
-    OPTION_INT('t', "timeout", "Timeout in seconds",
-                DEFAULT(30),
-                RANGE(1, 60),
-                HANDLER(custom_handler)),
+    OPTION_INT('t', "timeout", "Timeout in seconds"),
     OPTION_INT('b', "beta", "Beta value", DEFAULT(42)),
     POSITIONAL_STRING("file", "File to process"),
 );
+
 
 int main(int argc, char **argv)
 {
@@ -46,5 +40,7 @@ int main(int argc, char **argv)
     printf("beta: %d\n", cargs_get_value(cargs, "beta").as_int);
     printf(".beta: %d\n", cargs_get_value(cargs, ".beta").as_int);
     printf("toto.beta: %d\n", cargs_get_value(cargs, "toto.beta").as_int);
+
+    cargs_free(&cargs);
 	return (0);
 }
